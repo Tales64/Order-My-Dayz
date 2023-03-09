0118aa5db4c1e330f4534055495578bad7db0b38
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-03-09 00:26:42 -0800

    all timeblock local storage works

--- Develop/script.js
+++ Develop/script.js
@@ -24,16 +24,104 @@ $(function () {
   function getLocal () {
   var user = localStorage.getItem(`hourlyTask9`)
   $(`.description-9`).append(user);
-  }
+  
+  var user = localStorage.getItem(`hourlyTask10`)
+  $(`.description-10`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask11`)
+  $(`.description-11`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask12`)
+  $(`.description-12`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask13`)
+  $(`.description-13`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask14`)
+  $(`.description-14`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask15`)
+  $(`.description-15`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask16`)
+  $(`.description-16`).append(user);
+  
+  var user = localStorage.getItem(`hourlyTask17`)
+  $(`.description-17`).append(user);
+}
   // ===============================================================================
   // storag arrea
 
-  $(`.saveBtn-9`).on("click", function(event){
+  $(`.9`).on("click", function(event){
     event.preventDefault();
     var user = localStorage.getItem(`hourlyTask9`)
     $(`.description-9`).append(user);
     var hourlyTask9 = $(`.description-9`).val();
     localStorage.setItem(`hourlyTask9`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.10`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask10`)
+    $(`.description-10`).append(user);
+    var hourlyTask9 = $(`.description-10`).val();
+    localStorage.setItem(`hourlyTask10`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.11`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask11`)
+    $(`.description-11`).append(user);
+    var hourlyTask9 = $(`.description-11`).val();
+    localStorage.setItem(`hourlyTask11`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.12`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask12`)
+    $(`.description-12`).append(user);
+    var hourlyTask9 = $(`.description-12`).val();
+    localStorage.setItem(`hourlyTask12`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.13`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask13`)
+    $(`.description-13`).append(user);
+    var hourlyTask9 = $(`.description-13`).val();
+    localStorage.setItem(`hourlyTask13`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.14`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask14`)
+    $(`.description-14`).append(user);
+    var hourlyTask9 = $(`.description-14`).val();
+    localStorage.setItem(`hourlyTask14`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.15`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask15`)
+    $(`.description-15`).append(user);
+    var hourlyTask9 = $(`.description-15`).val();
+    localStorage.setItem(`hourlyTask15`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.16`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask16`)
+    $(`.description-16`).append(user);
+    var hourlyTask9 = $(`.description-16`).val();
+    localStorage.setItem(`hourlyTask16`, hourlyTask9);
+    console.log(hourlyTasks)
+})
+  $(`.17`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`hourlyTask17`)
+    $(`.description-17`).append(user);
+    var hourlyTask9 = $(`.description-17`).val();
+    localStorage.setItem(`hourlyTask17`, hourlyTask9);
     console.log(hourlyTasks)
 })
     
